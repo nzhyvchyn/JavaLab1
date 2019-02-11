e567d1349dc8c69c776df5f217ac1c39f44ff454
nzhyvchyn <44567357+nzhyvchyn@users.noreply.github.com>
2019-02-11 17:26:48 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Sofa sofa1 = new Sofa();
         Sofa sofa2 = new Sofa("Name", 5, 2, "beige");
-        Sofa sofa3 = new Sofa("Name1", 10, 5 , "matte");
+        Sofa sofa3 = new Sofa("Name1", 10, 5 , "matte", 2, "cotton", 3, 200, "US");
 
 
         System.out.println(sofa1.toString());
@@ -24,7 +24,7 @@ public class Main {
         sofa2.printStaticPillows();
         sofa3.printStaticPillows();
 
-        sofa2.resetValues("Name2", 4, 3, "white", 10, "polyester", 2, 300, "Ukraine");
+
     }
 }
 

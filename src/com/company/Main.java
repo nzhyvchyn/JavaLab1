7829a7c007512cefa191fa6342e091c1676df5fb
nzhyvchyn <44567357+nzhyvchyn@users.noreply.github.com>
2019-02-11 16:04:56 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,10 +10,21 @@ public class Main {
 
         System.out.println(sofa1.toString());
         System.out.println(sofa2.toString());
-        System.out.println(sofa3.toString());
-        sofa1.printFieldPillows();
-        sofa1.printStaticPillows();
 
+        sofa2.resetValues("Name2", 4, 3, "white", 10, "polyester", 2, 300, "Ukraine");
+
+        System.out.println(sofa2.toString());
+        System.out.println(sofa3.toString());
+
+        sofa1.printPillows();
+        sofa2.printPillows();
+        sofa3.printPillows();
+
+        sofa1.printStaticPillows();
+        sofa2.printStaticPillows();
+        sofa3.printStaticPillows();
+
+        sofa2.resetValues("Name2", 4, 3, "white", 10, "polyester", 2, 300, "Ukraine");
     }
 }
 
